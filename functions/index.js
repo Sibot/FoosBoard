34729e327fa8081a2807a86c7a33d551f3103ca6
Tobias Axelsson <msn@tobis.se>
2018-03-20 22:25:38 +0100

    clean up of debug logging

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
+
 admin.initializeApp(functions.config().firebase)
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -14,18 +15,15 @@ exports.segregateGameTeams = functions.database
     const game = event.data.val()
     var gameId = event.params.id
 
-    console.info('parsing game:', game, gameId)
-
     return new Promise((resolve, reject) => {
-      game.teams.forEach((team, i, a) => {
-        console.info('parsing team: ', team)
-        team.players.forEach((player, i, a) => {
-          console.info('parsing player: ', player)
+      game.teams.forEach(team => {
+        team.players.forEach(player => {
           admin
             .database()
             .ref('/teamSetup')
             .push({
               gameId: gameId,
+              playedAt: game.playedAt,
               playerId: player.key,
               team: team.id,
               score: team.score,
@@ -43,9 +41,6 @@ exports.calculatePlayerWins = functions.database
     // Grab the current value of what was written to the Realtime Database.
     const teamSetup = event.data.val()
 
-    console.info('the original', teamSetup)
-    console.info('Calculating wins for player: ', teamSetup.playerId)
-
     var currentPlayer = {
       totalPlayed: 0,
       totalWon: 0
@@ -58,30 +53,25 @@ exports.calculatePlayerWins = functions.database
         .orderByChild('playerId')
         .equalTo(teamSetup.playerId)
         .once('value', games => {
-          console.info('counting games')
           games.forEach(gameSnap => {
             var game = gameSnap.val()
+
             currentPlayer.totalPlayed += 1
-            console.info(
-              'counting game: ',
-              gameSnap.key,
-              currentPlayer.totalPlayed
-            )
+
             if (game.isWinner) {
               currentPlayer.totalWon += 1
-              console.info('count winner of game:', currentPlayer.totalWon)
             }
           })
           resolve()
         })
     }).then(() => {
       var update = {}
+
       update['/players/' + teamSetup.playerId + '/totalPlayed'] =
         currentPlayer.totalPlayed
+
       update['/players/' + teamSetup.playerId + '/totalWon'] =
         currentPlayer.totalWon
-
-      console.info('Updating player ', currentPlayer)
 
       return admin
         .database()
