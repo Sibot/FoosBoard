0b39942382d36c73eacaa92bbec7391b592ad64f
Tobias Axelsson <msn@tobis.se>
2018-03-21 23:59:37 +0100

    separate a Game component

--- src/components/Index/GamesList.vue
+++ src/components/Index/GamesList.vue
@@ -6,41 +6,23 @@
       <v-card-text>
         <v-layout row wrap>
           <v-flex v-for="game in topTenLatestGames" :key="game.key">
-            <v-card>
-              <v-card-title>
-                {{game.playedAt}}
-              </v-card-title>
-              <v-card-text>
-                <v-list two-line>
-                  <template v-for="team in sortByScore(game.teams)">
-                    <v-list-tile :key="team.id">
-                      <v-list-tile-content>
-                        <v-list-tile-title >
-                          <span>Players:
-                            <span v-for="player in team.players" :key="player.id">{{player.name}} </span>
-                          </span>
-                        </v-list-tile-title>
-                        <v-list-tile-sub-title>
-                          <span>Score: {{team.score}}</span>
-                        </v-list-tile-sub-title>
-                      </v-list-tile-content>
-                    </v-list-tile>
-                  </template>
-                </v-list>
-              </v-card-text>
-            </v-card>
+            <app-game :game="game">
+            </app-game>
           </v-flex>
         </v-layout>
       </v-card-text>
     </v-card>
   </v-container>
-
 </template>
 <script>
 import { mapGetters } from 'vuex'
+import Game from '../Shared/Game'
 
 export default {
   name: 'GamesList',
+  components: {
+    'app-game': Game
+  },
   data () {
     return {}
   },
---
+++ src/components/Shared/Game.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card>
+    <v-card-title>
+      {{game.playedAt}}
+    </v-card-title>
+    <v-card-text>
+      <v-list two-line>
+        <template v-for="team in sortByScore(game.teams)">
+          <v-list-tile :key="team.id">
+            <v-list-tile-content>
+              <v-list-tile-title >
+                <span>Players:
+                  <span v-for="player in team.players" :key="player.id">{{player.name}} </span>
+                </span>
+              </v-list-tile-title>
+              <v-list-tile-sub-title>
+                <span>Score: {{team.score}}</span>
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Game',
+  props: {
+    game: Object
+  }
+}
+</script>
+
