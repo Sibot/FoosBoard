b20ab38309ec2877f22593e97f83b9d933eef535
Tobias Axelsson <msn@tobis.se>
2018-04-08 13:06:07 +0200

    provide functions for router beforeEnter actions

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from '../vuex/index'
 import Index from '@/components/Index/Index'
 import About from '@/components/About'
 import Events from '@/components/Events'
@@ -7,10 +8,57 @@ import NewGame from '@/components/NewGame/NewGame'
 import SignIn from '@/components/User/SignIn'
 import SignUp from '@/components/User/SignUp'
 import Profile from '@/components/User/Profile'
-import store from '../vuex/index'
 
 Vue.use(Router)
 
+async function checkAuthState (to, from, next) {
+  const isAuthenticated = await getAuthState()
+  if (isAuthenticated === null) {
+    next('/profile')
+  } else {
+    next()
+  }
+}
+async function checkLoginState (to, from, next) {
+  const isLoggedIn = await getLoginState()
+  if (isLoggedIn === null) {
+    next('/signin')
+  } else {
+    next()
+  }
+}
+
+function getAuthState () {
+  return new Promise((resolve, reject) => {
+    if (store.state.isAuthenticated === null) {
+      const unwatch = store.watch(
+        () => store.state.isAuthenticated,
+        value => {
+          unwatch()
+          resolve(value)
+        }
+      )
+    } else {
+      resolve(store.state.isAuthenticated)
+    }
+  })
+}
+
+function getLoginState () {
+  return new Promise((resolve, reject) => {
+    if (store.state.isLoggedIn === null) {
+      const unwatch = store.watch(
+        () => store.state.isLoggedIn,
+        value => {
+          unwatch()
+          resolve(value)
+        }
+      )
+    } else {
+      resolve(store.state.isLoggedIn)
+    }
+  })
+}
 export default new Router({
   mode: 'history',
   routes: [
@@ -28,37 +76,19 @@ export default new Router({
       path: '/game',
       name: 'NewGame',
       component: NewGame,
-      beforeEnter: (to, from, next) => {
-        if (store.getters.isAuthenticated) {
-          next()
-        } else {
-          next('/signin')
-        }
-      }
+      beforeEnter: checkAuthState
     },
     {
       path: '/events',
       name: 'Events',
       component: Events,
-      beforeEnter: (to, from, next) => {
-        if (store.getters.isAuthenticated) {
-          next()
-        } else {
-          next('/signin')
-        }
-      }
+      beforeEnter: checkAuthState
     },
     {
       path: '/profile',
       name: 'Profile',
       component: Profile,
-      beforeEnter: (to, from, next) => {
-        if (store.getters.isLoggedIn) {
-          next()
-        } else {
-          next('/signin')
-        }
-      }
+      beforeEnter: checkLoginState
     },
     {
       path: '/signup',
