fdd92bd468c3c9142e61cf7ff78bd481ff75c8f0
Tobias Axelsson <msn@tobis.se>
2018-04-09 22:20:44 +0200

    add foos icons to about page

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,5 +2,15 @@
   <v-container>
     Thanks to <a href="https://thenounproject.com">thenounproject</a> for providing foosball icons!
     Icons used was Created by Milky - Digital innovation from the Noun Project
+            <img src="@/assets/foosballplayer.svg" class="icon" alt="Stilized foosball player">
+
+            <img src="@/assets/foos.png" class="icon" alt="Stilized foosball player">
   </v-container>
 </template>
+<style scoped>
+.icon {
+  width: 50px;
+  height: 50px;
+}
+</style>
+
