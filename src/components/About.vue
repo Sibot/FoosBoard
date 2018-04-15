0004c550b8e7658a396d31d53bfdea1ac1d97e88
Tobias Axelsson <msn@tobis.se>
2018-04-15 22:34:05 +0200

    add html structure

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,15 +1,24 @@
 <template>
   <v-container>
-    Thanks to <a href="https://thenounproject.com">thenounproject</a> for providing foosball icons!
-    Icons used was Created by Milky - Digital innovation from the Noun Project
-            <img src="@/assets/foos.svg" class="icon" alt="Stilized foosball player">
-            <img src="@/assets/foos.png" class="icon" alt="Stilized foosball player">
+    <v-flex>
+      <v-card>
+        <v-card-title>
+          <h2 class="headline text-xs-center">Creds</h2>
+        </v-card-title>
+        <v-card-text>
+          Thanks to <a href="https://thenounproject.com">thenounproject</a> for providing foosball icons!
+          Icons used was Created by Milky - Digital innovation from the Noun Project<br />
+          <img src="@/assets/foos.svg" class="icon" alt="Stilized foosball player">
+          <img src="@/assets/foos.png" class="icon" alt="Stilized foosball player">
+        </v-card-text>
+      </v-card>
+    </v-flex>
   </v-container>
 </template>
 <style scoped>
 .icon {
-  width: 50px;
-  height: 50px;
+  width: 75px;
+  height: 75px;
 }
 </style>
 
