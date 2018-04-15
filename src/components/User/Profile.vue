74a35df094d816c5bbb51f3d21a8919a2efe3bc0
Tobias Axelsson <github@tobis.se>
2018-04-15 19:42:39 +0200

    Fix profile container

--- src/components/User/Profile.vue
+++ src/components/User/Profile.vue
@@ -1,36 +1,42 @@
 <template>
   <v-container fluid>
     <v-layout column align-center>
-      <h2>Profile settings</h2>
-      <v-form v-if="!user.emailVerified">
-          <h3>Greetings {{displayName}}!</h3>
-          <p>You need to verify your email '{{user.email}}'.</p>
-          <v-btn v-if="!isVerificationEmailSent" @click="sendVerificationEmail" :color="isVerificationEmailSent ? 'success' : 'warning'">Send verification email</v-btn>
-          <p v-if="isVerificationEmailSent">Refresh this page after you have verified your email</p>
-      </v-form>
-      <v-form>
-        <v-text-field
-          label="Display name"
-          name="displayName"
-          v-model="displayName">
-        </v-text-field>
-        <v-switch
-          label="Enable Notifications"
-          v-model="isNotificationsAllowed"
-          @click="notify"
-        ></v-switch>
-        <v-layout row>
-          <v-avatar size='50' v-show="avatarUrl" class="mr-3">
-            <img :src="avatarUrl" alt="A user avatar">
-          </v-avatar>
-          <v-text-field
-          label="Avatar URL"
-          name="avatar"
-          v-model="avatarUrl">
-        </v-text-field>
-        </v-layout>
-        <v-btn @click="setProfile" color="info" :disabled="isSaving">Update Profile</v-btn>
-      </v-form>
+        <v-card>
+          <v-container>
+            <h2>Profile settings</h2>
+            <v-form v-if="!user.emailVerified">
+                <h3>Greetings {{displayName}}!</h3>
+                <p>You need to verify your email '{{user.email}}'.</p>
+                <v-btn v-if="!isVerificationEmailSent" @click="sendVerificationEmail" :color="isVerificationEmailSent ? 'success' : 'warning'">Send verification email</v-btn>
+                <p v-if="isVerificationEmailSent">Refresh this page after you have verified your email</p>
+            </v-form>
+            <v-form>
+                <v-text-field
+                  label="Display name"
+                  name="displayName"
+                  v-model="displayName">
+                </v-text-field>
+                <v-switch
+                  label="Enable Notifications"
+                  v-model="isNotificationsAllowed"
+                  @click="notify"
+                ></v-switch>
+                <v-layout row>
+                  <v-avatar size='50' v-show="avatarUrl" class="mr-3">
+                    <img :src="avatarUrl" alt="A user avatar">
+                  </v-avatar>
+                  <v-text-field
+                  label="Avatar URL"
+                  name="avatar"
+                  v-model="avatarUrl">
+                </v-text-field>
+                </v-layout>
+                <v-btn @click="setProfile" color="info" :disabled="isSaving">
+                  Update Profile
+                </v-btn>
+            </v-form>
+          </v-container>
+        </v-card>
     </v-layout>
   </v-container>
 </template>
